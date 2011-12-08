bb957e365d1a48d3eb852be286d9d3920e9b88ba
Mike Steinert <mike.steinert@gmail.com>
2011-12-07 20:02:30 -0700

    Remove extraneous "inline"s
    Signed-off-by: Mike Steinert <mike.steinert@gmail.com>

--- joy/timespec.h
+++ joy/timespec.h
@@ -20,83 +20,83 @@
 G_BEGIN_DECLS
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_gettime(struct timespec *x, clockid_t id);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_add(struct timespec *x, const struct timespec *y);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_add_seconds(struct timespec *x, gulong sec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_add_milliseconds(struct timespec *x, gulong msec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_add_microseconds(struct timespec *x, gulong usec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_add_nanoseconds(struct timespec *x, gulong nsec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_subtract(struct timespec *x, const struct timespec *y);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_subtract_seconds(struct timespec *x, gulong sec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_subtract_milliseconds(struct timespec *x, gulong msec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_subtract_microseconds(struct timespec *x, gulong usec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_subtract_nanoseconds(struct timespec *x, gulong nsec);
 
 G_GNUC_INTERNAL
-inline gint
+gint
 joy_timespec_compare(const struct timespec *x, const struct timespec *y);
 
 G_GNUC_INTERNAL
-inline gdouble
+gdouble
 joy_timespec_seconds(const struct timespec *x);
 
 G_GNUC_INTERNAL
-inline gulong
+gulong
 joy_timespec_milliseconds(const struct timespec *x);
 
 G_GNUC_INTERNAL
-inline gulong
+gulong
 joy_timespec_microseconds(const struct timespec *x);
 
 G_GNUC_INTERNAL
-inline gulong
+gulong
 joy_timespec_nanoseconds(const struct timespec *x);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_from_seconds(struct timespec *x, gulong sec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_from_milliseconds(struct timespec *x, gulong msec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_from_microseconds(struct timespec *x, gulong usec);
 
 G_GNUC_INTERNAL
-inline struct timespec *
+struct timespec *
 joy_timespec_from_nanoseconds(struct timespec *x, gulong nsec);
 
 G_END_DECLS
